66c0e37ef878322ea315f3c415c04b1c5bf0599f
Xiao Fan Ding <faneding@hotmail.com>
2023-05-12 21:57:59 -0600

    Update Step1-tomos-split.py

--- Step1-tomos-split.py
+++ Step1-tomos-split.py
@@ -1,64 +1,66 @@
-import os, argparse, shutil
+import os
+import subprocess
 import numpy as np
-from PIL import Image
+import shutil
+import sys
 from datetime import datetime
+run = 1
+starttime = datetime.now()
+totaltime = datetime.now()
+present_dir = os.getcwd()
+
+def root_path():
+	return os.path.abspath(os.sep)
+
+def progressbar(it, prefix="", size=50, out=sys.stdout):
+	count = len(it)
+	def show(j):
+		x = int(size*j/count)
+		out.write("%s[%s%s] %i/%i\r" % (prefix, u"#"*x, "."*(size-x), j, count))
+		out.flush()        
+	show(0)
+	for i, item in enumerate(it):
+		yield item
+		show(i+1)
+	out.write("\n")
+	out.flush()
 
 # Path to directory containing TIFF images
 parser = argparse.ArgumentParser(description='Split the tomos for sub-reconstruction')
-parser.add_argument('-image_raw_path', type=str, required = True,
-                    help='Path including tomo/ flats/ darks/ folder')
-parser.add_argument('-image_save_path', type=str, required = True,
-                    help='Path for saving the splitted tomo files')
-parser.add_argument('-m', type=int, default=2, help='Number of sections')
-
+parser.add_argument('-image_raw_path', type=str, required = True, help='Path including flats, darks, and tomo folders')
+parser.add_argument('-image_save_path', type=str, required = True, help='Path for saving the splitted tomo files')
+parser.add_argument('-m', type=int, default=2, help='Number of splits')
 args, unparsed = parser.parse_known_args()
 
+PATH = args.image_raw_path
+SAVE = args.image_save_path
+splits = args.m
 
-starttime = datetime.now()
+# run only if tomo darks and flats folders are present
+if os.path.isdir(PATH+"/tomo")+os.path.isdir(PATH+"/flats")+os.path.isdir(PATH+"/darks") == 3:
+	if not os.path.isdir(SAVE):
+		os.mkdir(SAVE)
 
+	# get number of files inside the desired directory
+	files = sorted([f for f in os.listdir(os.path.join(PATH,"tomo")) if os.path.isfile(os.path.join(PATH,"tomo",f))])
+	filesnum = np.size(files)
 
-# Number of sections to split the images into
-# create the output directories
-for i in range(1, args.m+1):
-    os.makedirs(os.path.join(args.image_save_path, f"section_{i}/tomo/"), exist_ok=False)
-    os.makedirs(os.path.join(args.image_save_path, f"section_{i}/flats/"), exist_ok=False)
-    os.makedirs(os.path.join(args.image_save_path, f"section_{i}/darks/"), exist_ok=False)
+	for i in range(splits):
+		# make output folder
+		if not os.path.isdir(os.path.join(SAVE,str(i).zfill(4))):
+			os.mkdir(os.path.join(SAVE,str(i).zfill(4)))
+			os.mkdir(os.path.join(SAVE,str(i).zfill(4),"tomo"))
+		# copy every i-th file
+		# for j in np.arange(i,filesnum,splits):
+		for j in progressbar(np.arange(i,filesnum,splits), "Making split #"+str(i)+": "):
+			# print(files[j])
+			shutil.copy(os.path.join(PATH,"tomo",files[j]), os.path.join(SAVE,str(i).zfill(4),"tomo",files[j]))
+		os.system("cp -r "+PATH+"/flats "+os.path.join(SAVE,str(i).zfill(4),"flats"))
+		os.system("cp -r "+PATH+"/darks "+os.path.join(SAVE,str(i).zfill(4),"darks"))
 
-# get the tomo list of tiff files in the input directory
-tomo_files = sorted([f for f in os.listdir(os.path.join(args.image_raw_path, f"tomo/")) if f.endswith(".tif")])
-if len(tomo_files) == 0:
-    print("No tif files in the tomo folder, please check your path")
-
-# split the images into sections and save them to the output directories
-for i, file_name in enumerate(tomo_files):
-    print(f'Now processing tomo index: {i}/{len(tomo_files)-1}')
-    section_num = (i % args.m) + 1
-    output_path = os.path.join(args.image_save_path, f"section_{section_num}/tomo/", file_name)
-    img = Image.open(os.path.join(args.image_raw_path, f"tomo/", file_name))
-    img.save(output_path)
-
-# get the flats list of tiff files in the input directory
-flats_files = sorted([f for f in os.listdir(os.path.join(args.image_raw_path, f"flats/")) if f.endswith(".tif")])
-if len(flats_files) == 0:
-    print("No tif files in the flat folder, please check your path")
-
-for i in range(1, args.m+1):
-    for f in flats_files:
-        source_file = os.path.join(args.image_raw_path, f"flats/",f)
-        dest_file = os.path.join(args.image_save_path, f"section_{i}/flats/",f)
-        shutil.copyfile(source_file, dest_file)
-
-# get the darks list of tiff files in the input directory
-darks_files = sorted([f for f in os.listdir(os.path.join(args.image_raw_path, f"darks/")) if f.endswith(".tif")])
-if len(darks_files) == 0:
-    print("No tif files in the flat folder, please check your path")
-
-for i in range(1, args.m+1):
-    for f in darks_files:
-        source_file = os.path.join(args.image_raw_path, f"darks/",f)
-        dest_file = os.path.join(args.image_save_path, f"section_{i}/darks/",f)
-        shutil.copyfile(source_file, dest_file)
-
+# otherwise notify that raw data is incomplete
+else:
+	print("Incomplete raw data folder")
 
 elapsedtime = str(datetime.now() - starttime)
 print("Finisehd in " + elapsedtime)
