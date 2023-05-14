ec9ce4465b18a2cafaa7ffb5e7cf7d1ddc1fa0d5
xfding57 <faneding@hotmail.com>
2023-05-14 01:40:39 -0600

    Moved functions to XFDing.py and follows previous naming convention

--- Step1-tomos-split.py
+++ Step1-tomos-split.py
@@ -1,29 +1,15 @@
 import os
+import argparse
 import subprocess
 import numpy as np
 import shutil
 import sys
+from XFDing import progressbar2 as progressbar
 from datetime import datetime
 run = 1
 starttime = datetime.now()
 totaltime = datetime.now()
 present_dir = os.getcwd()
-
-def root_path():
-	return os.path.abspath(os.sep)
-
-def progressbar(it, prefix="", size=50, out=sys.stdout):
-	count = len(it)
-	def show(j):
-		x = int(size*j/count)
-		out.write("%s[%s%s] %i/%i\r" % (prefix, u"#"*x, "."*(size-x), j, count))
-		out.flush()        
-	show(0)
-	for i, item in enumerate(it):
-		yield item
-		show(i+1)
-	out.write("\n")
-	out.flush()
 
 # Path to directory containing TIFF images
 parser = argparse.ArgumentParser(description='Split the tomos for sub-reconstruction')
@@ -47,16 +33,16 @@ if os.path.isdir(PATH+"/tomo")+os.path.isdir(PATH+"/flats")+os.path.isdir(PATH+"
 
 	for i in range(splits):
 		# make output folder
-		if not os.path.isdir(os.path.join(SAVE,str(i).zfill(4))):
-			os.mkdir(os.path.join(SAVE,str(i).zfill(4)))
-			os.mkdir(os.path.join(SAVE,str(i).zfill(4),"tomo"))
+		if not os.path.isdir(os.path.join(SAVE,"section_"str(i+1))):
+			os.mkdir(os.path.join(SAVE,"section_"str(i+1)))
+			os.mkdir(os.path.join(SAVE,"section_"str(i+1),"tomo"))
 		# copy every i-th file
 		# for j in np.arange(i,filesnum,splits):
 		for j in progressbar(np.arange(i,filesnum,splits), "Making split #"+str(i)+": "):
 			# print(files[j])
-			shutil.copy(os.path.join(PATH,"tomo",files[j]), os.path.join(SAVE,str(i).zfill(4),"tomo",files[j]))
-		os.system("cp -r "+PATH+"/flats "+os.path.join(SAVE,str(i).zfill(4),"flats"))
-		os.system("cp -r "+PATH+"/darks "+os.path.join(SAVE,str(i).zfill(4),"darks"))
+			shutil.copy(os.path.join(PATH,"tomo",files[j]), os.path.join(SAVE,"section_"str(i+1),"tomo",files[j]))
+		os.system("cp -r "+PATH+"/flats "+os.path.join(SAVE,"section_"str(i+1),"flats"))
+		os.system("cp -r "+PATH+"/darks "+os.path.join(SAVE,"section_"str(i+1),"darks"))
 
 # otherwise notify that raw data is incomplete
 else:
