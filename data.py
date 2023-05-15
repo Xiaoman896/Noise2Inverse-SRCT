dbc498dcfa5c19013273454f568b3ba88681675a
xfding57 <faneding@hotmail.com>
2023-05-15 01:20:54 -0600

    Update

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ import numpy as np
 import h5py, threading
 import queue as Queue
 import h5py, glob
+import os
+from XFDing import read_images
 from util import scale2uint8
 
 class bkgdGen(threading.Thread):
@@ -64,5 +66,35 @@ def get1batch4test(dsfn, in_depth):
 
     return batch_X.astype(np.float32) , batch_Y.astype(np.float32)
 
+def gen_train_batch_bg_from_folder(dsfn, mb_size, in_depth, img_size):
+    X = read_images(os.path.join(dsfn,"train_ns")).astype(np.float32)
+    Y = read_images(os.path.join(dsfn,"train_gt")).astype(np.float32)
+
+    while True:
+        idx = np.random.randint(0, X.shape[0]-in_depth, mb_size)
+        if img_size == X.shape[1]:
+            # preallocate splace with zero array
+            rst, cst = np.zeros(mb_size, dtype=np.int), np.zeros(mb_size, dtype=np.int)
+        else:
+            rst = np.random.randint(0, X.shape[1]-img_size, mb_size)
+            cst = np.random.randint(0, X.shape[2]-img_size, mb_size)
+
+        batch_X = np.array([np.transpose(X[s_idx : (s_idx+in_depth)], (1, 2, 0)) for s_idx in idx])
+        batch_X = [batch_X[_i, _r:_r+img_size, _c:_c+img_size, :] for _i, _r, _c in zip(range(mb_size), rst, cst)]
+
+        batch_Y = np.expand_dims([Y[s_idx+in_depth//2] for s_idx in idx], 3)
+        batch_Y = [batch_Y[_i, _r:_r+img_size, _c:_c+img_size, :] for _i, _r, _c in zip(range(mb_size), rst, cst)]
+
+        yield np.array(batch_X), np.array(batch_Y)
+
+def get1batch4test_from_folder(dsfn, in_depth):
+    X = read_images(os.path.join(dsfn,"test_ns")).astype(np.float32)
+    Y = read_images(os.path.join(dsfn,"test_gt")).astype(np.float32)
+
+    idx = (X.shape[0]-in_depth, ) # always use slice in_depth//2 for validation
+    batch_X = np.array([np.transpose(X[s_idx : (s_idx+in_depth)], (1, 2, 0)) for s_idx in idx])
+    batch_Y = np.expand_dims([Y[s_idx+in_depth//2] for s_idx in idx], 3)
+
+    return batch_X.astype(np.float32) , batch_Y.astype(np.float32)
 
 
